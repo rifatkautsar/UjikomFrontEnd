252106f475c51a368937d5430917347347099856
kautsarrifat <kautsarrifat@gmail.com>
2019-03-31 22:03:34 +0700

    zzzz

---
+++ app/src/main/java/com/example/latihanujikom/Login.java
@@ -0,0 +1,40 @@
+package com.example.latihanujikom;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class Login extends AppCompatActivity {
+
+    EditText Email, Password;
+    Button LogInButton, RegisterButton;
+    FirebaseAuth mAuth;
+    FirebaseAuth.AuthStateListener mAuthListner;
+    FirebaseUser mUser;
+    String email, password;
+    ProgressDialog dialog;
+    public static final String userEmail="";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        LogInButton = (Button) findViewById(R.id.b);
+    }
+}
